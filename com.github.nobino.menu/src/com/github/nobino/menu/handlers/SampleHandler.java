0bcd60dba7a4c721ca98bb63aaf6aa5b69944dae
nobino <nobino_github@outlook.com>
2013-05-07 22:18:14 +0900

    Show LocationURI of the selection if it is JavaElement.

--- com.github.nobino.menu/src/com/github/nobino/menu/handlers/SampleHandler.java
+++ com.github.nobino.menu/src/com/github/nobino/menu/handlers/SampleHandler.java
@@ -1,12 +1,18 @@
 package com.github.nobino.menu.handlers;
 
+import java.net.URI;
+import java.util.Iterator;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
 
 /**
  * Our sample handler extends AbstractHandler, an IHandler base class.
@@ -27,10 +33,29 @@ public class SampleHandler extends AbstractHandler {
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
 		ISelection currentSelection = HandlerUtil.getCurrentSelection(event);
+		
+		StringBuilder builder = new StringBuilder();
+		
+		
+		if (currentSelection instanceof IStructuredSelection) {
+			IStructuredSelection sSelection = (IStructuredSelection) currentSelection;
+			for (Iterator<?> iterator = sSelection.iterator(); iterator.hasNext();) {
+				Object type = (Object) iterator.next();
+				if (type instanceof IJavaElement) {
+					IJavaElement javaElement = (IJavaElement) type;
+					IResource resource = javaElement.getResource();
+					URI uri = resource.getLocationURI();
+					
+					builder.append(uri.toString());
+					builder.append(System.getProperty("line.separator"));
+				}
+			}
+		}
+		
 		MessageDialog.openInformation(
 				window.getShell(),
 				"リソースの情報を得るプラグイン",
-				currentSelection.toString());
+				builder.toString());
 		return null;
 	}
 }
